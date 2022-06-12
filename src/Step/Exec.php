9ed551768255e265c618e7c2906cffb3ca9ecdc6
Blaize Kaye <blaize.kaye@amazee.com>
2022-06-13 11:52:08 +1200

    Adds command substitution functionality

--- src/Step/Exec.php
+++ src/Step/Exec.php
@@ -5,12 +5,23 @@ namespace Migrator\Step;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
+/**
+ * This step will run a command either locally or via an exec
+ * It allows for the following textual substitutions
+ * %environment% = environment name
+ * %project% = project name
+ * %namespace% = k8s namespace
+ */
+
 class Exec extends StepParent {
+
     public function run(array $args)
     {
-        if(!empty($args['local']) && $args['local'] == "true" && !empty($args['command'])) {
+        $command = $this->doTextSubstitutions($args['command']);
 
-            $process = Process::fromShellCommandline($args['command']);
+        if(!empty($args['local']) && $args['local'] == "true" && !empty($command)) {
+
+            $process = Process::fromShellCommandline($command);
             $process->run();
 
             if (!$process->isSuccessful() || $process->getExitCode() != 0) {
@@ -21,9 +32,23 @@ class Exec extends StepParent {
             return;
         }
 
-        if(empty($args['deployment']) || empty($args['command'])) {
+        if(empty($args['deployment']) || empty($command)) {
             throw new \Exception("An Exec step requires `deployment` and `command` arguments");
         }
-        $this->utilityBelt->execInPod($args['deployment'], $args['command']);
+        $this->utilityBelt->execInPod($args['deployment'], $command);
+    }
+
+    protected function doTextSubstitutions($string)
+    {
+        $substitutions = [
+          '%project%' => $this->args->project,
+          '%environment%' => $this->args->environment,
+          '%namespace%' => $this->args->namespace,
+        ];
+
+        foreach ($substitutions as $key => $value) {
+            $string = str_replace($key, $value, $string);
+        }
+        return $string;
     }
 }
