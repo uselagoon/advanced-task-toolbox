dee6e386a1ed8dcc84b421af03da5ad2b818049b
Blaize Kaye <blaize.kaye@amazee.com>
2022-08-08 15:55:30 +1200

    Rollbacks exit 1 as failure

--- RoboFile.php
+++ RoboFile.php
@@ -73,6 +73,7 @@ class RoboFile extends \Robo\Tasks
                 $args->steps = $migration['rollback'];
                 $runner = new \Migrator\Runner($args);
                 $runner->run();
+                exit(1);
             }
         }
     }
